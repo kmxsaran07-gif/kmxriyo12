cbe5961cf1d6ce2c74980fe8248d3e2474791fe0
kmxsaran07-gif <kmxsaran07@gmail.com>
2025-12-18 01:48:29 +0530

    Add files via upload

---
+++ vars.py
@@ -0,0 +1,11 @@
+# SudoR2spr WOODcraft
+# Add your details here and then deploy by clicking on HEROKU Deploy button
+import os
+
+API_ID    = os.environ.get("API_ID", "28585138")
+API_HASH  = os.environ.get("API_HASH", "74b7afde7d9a546db3451cf4689f4e14")
+BOT_TOKEN = os.environ.get("BOT_TOKEN", "") 
+
+#WEBHOOK = True  # Don't change this
+#PORT = int(os.environ.get("PORT", 8080))  # Default to 8000 if not set
+
